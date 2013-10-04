69d86301f6020ca5f47f5afce1667d356f261dc3
shordeaux <shordeaux@waterproof.fr>
2013-10-04 16:19:11 +0200

    fix

--- lib/form/exportForm.class.php
+++ lib/form/exportForm.class.php
@@ -21,7 +21,7 @@ class exportForm extends sfForm {
             'to' => time()
             );
 
-		if ($sfUser->hasCredential('root')) {
+		if ($sfUser->hasCredential('admin')) {
             $widgets['operator'] = new sfWidgetFormInputText(array('label' => 'Agence'), array('class' => 'inp-form'));
             $defaults['operator'] = '';
 		} else {
